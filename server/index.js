6d939d1b214e9fa7ca40c6f625f2eee8b85f7e5f
Killshift <kshitijsharma8091@gmail.com>
2021-12-28 16:37:09 +0530

    Without style

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express=require('express')
+const app=express()
+const http=require('http')
+const {Server}=require('socket.io')
+const cors=require('cors')
+
+app.use(cors())
+const server=http.createServer(app)
+const io=new Server(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        methods:["GET","POST"]
+    },
+})
+
+io.on("connection", (socket)=>{
+    console.log(`User connected ${socket.id}`)
+    socket.on("join_room",(data)=>{
+     socket.join(data)
+     console.log(`User with ID ${socket.id} joined the room ${data}`)
+    })
+    socket.on("send_message",(data)=>{
+        socket.to(data.room).emit("recieve_message", data)
+    })
+    socket.on("disconnect",()=>{
+        console.log("User disconnected",socket.id)
+        })
+})
+
+server.listen(3001, ()=>{
+    console.log('server running')
+})
